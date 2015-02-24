5ffb85d9245f5daa3b62f5adc19f8af6dd57649e
STUDENT <student@localhost.localdomain>
2015-02-24 15:03:10 +0530

    super blog_app

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
-  get"home"
+  resources :blogs
+
+  get "home1" =>"home#home"
+  get "about" =>"home#about"
+  get "contact_us" =>"home#contact_us"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
